2002ed894a6e7626fc8d10f04068cd8836f924ef
thaonam <43461892+thaonam@users.noreply.github.com>
2026-09-13 22:46:48 +0700

    Add apps/api/src/modules/exams/exams.module.ts

---
+++ apps/api/src/modules/exams/exams.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Exam, ExamSchema } from '../../database/schemas/exam.schema';
+import { Question, QuestionSchema } from '../../database/schemas/question.schema';
+import { ExamsController } from './exams.controller';
+import { ExamsService } from './exams.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Exam.name, schema: ExamSchema },
+      { name: Question.name, schema: QuestionSchema },
+    ]),
+  ],
+  controllers: [ExamsController],
+  providers: [ExamsService],
+  exports: [ExamsService],
+})
+export class ExamsModule {}
