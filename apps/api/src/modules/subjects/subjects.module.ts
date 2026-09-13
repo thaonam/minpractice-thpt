4f8cc24fa1459f70a0431ecc868b41832dcec6be
thaonam <43461892+thaonam@users.noreply.github.com>
2026-09-13 23:49:42 +0700

    Update subjects module for auth guards

--- apps/api/src/modules/subjects/subjects.module.ts
+++ apps/api/src/modules/subjects/subjects.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Subject, SubjectSchema } from '../../database/schemas/subject.schema';
+import { AuthModule } from '../auth/auth.module';
 import { SubjectsController } from './subjects.controller';
 import { SubjectsService } from './subjects.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }])],
+  imports: [AuthModule, MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }])],
   controllers: [SubjectsController],
   providers: [SubjectsService],
+  exports: [SubjectsService],
 })
 export class SubjectsModule {}
