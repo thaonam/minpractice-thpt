8d911f3d4122ecf2168182b0018b92a98e611799
thaonam <43461892+thaonam@users.noreply.github.com>
2026-09-14 00:00:42 +0700

    Protect questions used by published exams

--- apps/api/src/modules/questions/questions.module.ts
+++ apps/api/src/modules/questions/questions.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Exam, ExamSchema } from '../../database/schemas/exam.schema';
 import { Question, QuestionSchema } from '../../database/schemas/question.schema';
 import { AuthModule } from '../auth/auth.module';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forFeature([{ name: Question.name, schema: QuestionSchema }])],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      { name: Question.name, schema: QuestionSchema },
+      { name: Exam.name, schema: ExamSchema },
+    ]),
+  ],
   controllers: [QuestionsController],
   providers: [QuestionsService],
   exports: [QuestionsService],
